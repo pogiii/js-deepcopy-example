3e14f59ab24e2e671a962ce31a1f63eeb19b3754
Aviv Ben Yosef <54292904+pogiii@users.noreply.github.com>
2021-11-30 01:39:35 +0200

    I made it work recursively too.

--- main.js
+++ main.js
@@ -1,29 +1,38 @@
+function deepCopy(obj) {
+    const keys = Object.keys(obj);
+    const values = Object.values(obj);
+
+    const newObj = {};
+
+
+    for (let i = 0; i < keys.length; i++) {
+
+        if (typeof values[i] == 'object') {
+            values[i] = deepCopy(values[i]);
+        }
+
+        newObj[keys[i]] = values[i];
+
+    }
+
+    return newObj;
+}
+
 const base = {
     a: 'b',
     b: 'c',
-    c: function () { return this.a }
+    c: function () { return this.a },
+    d: {
+        skills: {
+            primary: 'code',
+            secondary: 'breath'
+        }
+    }
 };
 
-const keys = Object.keys(base);
-const values = Object.values(base);
+const newObj = deepCopy(base);
 
-const newObj = {};
+base.d.skills.primary = 'drawings';
 
-
-for (let i = 0; i < keys.length; i++) {
-
-    newObj[keys[i]] = values[i];
-
-}
-
-function changeA(obj, newVal) {
-    obj.a = newVal;
-
-    return obj
-}
-
-changeA(newObj, 'Deep');
-
-
-console.log(base.c());
-console.log(newObj.c()); // Deep Clone!
+console.log(base);
+console.log(newObj);
